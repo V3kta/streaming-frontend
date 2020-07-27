41c519a5d02b315faa8d9bc3e2ce2350f658212d
V3kta <luca.rasche@outlook.com>
2020-07-27 11:58:37 +0200

    zwischenstand

---
+++ src/app/Model/Serie.ts
@@ -0,0 +1,15 @@
+export class Serie {
+  serie_id: number;
+
+  name: string;
+
+  beschreibung: string;
+
+  // Zuletzt gesehene Folge
+  zgFolge: number;
+
+  // Zuletzt gesehene Staffel
+  zgStaffel: number;
+
+  zgDatum: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'streaming-service';
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'name',
+    'zgFolge',
+    'zgStaffel',
+    'zgDatum',
+    'beschreibung',
+  ];
 }
