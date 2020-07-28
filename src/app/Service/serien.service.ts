ad409075b765371726a7280cf474a4214808df35
V3kta <luca.rasche@outlook.com>
2020-07-28 13:03:26 +0200

    zwischenstand

--- src/app/Service/serien.service.ts
+++ src/app/Service/serien.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Serie } from 'src/app/model/Serie';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SerienService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  refresh(): Observable<Serie[]> {
+    return this.http.get<Serie[]>('http://localhost:8080/get');
+  }
+
+  save(serienList: Serie[]): void {
+    this.http.post<Serie[]>('http://localhost:8080/get', serienList);
+  }
+
+  delete(serie: Serie): void {
+    this.http.post<Serie>('http://localhost:8080/get', serie);
+  }
 }
